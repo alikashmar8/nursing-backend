37ff80c482ab5ced2102bbf6baafec4dc7752567
alikashmar8 <alikashmar8@gmail.com>
2025-02-05 23:12:40 +0200

    Add @nestjs/serve-static package and configure ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from 'config/configuration';
+import { join } from 'path';
 
 import { AddressesModule } from './addresses/addresses.module';
 import { AppController } from './app.controller';
@@ -28,6 +30,10 @@ import { UsersModule } from './users/users.module';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) =>
         configService.get('database'),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../public'), // added ../ to get one folder back
+      serveRoot: '/public/', //last slash is important
     }),
     AddressesModule,
     AuthModule,
