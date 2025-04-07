5e3376adfac226979a277a379440ccab54757829
alikashmar8 <alikashmar8@gmail.com>
2025-04-07 21:36:00 +0300

    Enhance ShiftsModule to include ReservationsService in providers for improved reservation handling

--- src/shifts/shifts.module.ts
+++ src/shifts/shifts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
-import { Reservation } from 'src/reservations/entities/reservation.entity';
-import { User } from 'src/users/entities/user.entity';
-import { UsersService } from 'src/users/users.service';
+import { DeviceToken } from '../device-tokens/entities/device-token.entity';
+import { Reservation } from '../reservations/entities/reservation.entity';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { ReservationsService } from './../reservations/reservations.service';
 import { Shift } from './entities/shift.entity';
 import { ShiftsController } from './shifts.controller';
 import { ShiftsService } from './shifts.service';
@@ -11,6 +12,6 @@ import { ShiftsService } from './shifts.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Shift, Reservation, User, DeviceToken])],
   controllers: [ShiftsController],
-  providers: [ShiftsService, UsersService],
+  providers: [ShiftsService, UsersService, ReservationsService],
 })
 export class ShiftsModule {}
